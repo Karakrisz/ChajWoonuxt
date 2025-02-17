99714130a44b26822febafa30cb94ca24b876ffe
Boaz <webevolution13@gmail.com>
2025-02-17 13:08:43 +0100

    upDate
    front end

---
+++ woonuxt_base/app/components/generalElements/AppHeader.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const links = [
+  { name: 'Rólunk', link: '/', prefetch: true },
+  { name: 'Galéria', link: '/' },
+  { name: 'Étlap', link: '/', prefetch: true },
+  { name: 'Kapcsolat', link: '/' },
+];
+</script>
+
+<template>
+  <header class="z-50 sticky top-0 bg-white">
+    <div class="container flex justify-between items-center py-4 gap-10">
+      <MenuTrigger class="lg:hidden" />
+
+      <NuxtLink to="/" class="shrink-0">
+        <NuxtImg src="/img/logo.svg" class="" />
+      </NuxtLink>
+      <div class="space-x-16 lg:flex items-center hidden">
+        <NuxtLink v-for="link in links" :key="link.name" :to="link.link" class="">{{ link.name }}</NuxtLink>
+      </div>
+      <div class="flex gap-4">
+        <ProductSearch class="hidden md:inline-flex max-w-[320px]" />
+        <SignInLink />
+        <CartTrigger />
+      </div>
+    </div>
+  </header>
+</template>
